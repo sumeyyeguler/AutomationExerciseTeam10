8490119e6e01040b5cb7f49fc21b8ac1b35e7550
Sumeyye Guler <sumeyyeguler0602@gmail.com>
2022-09-28 18:23:21 +0300

    28.09.2022

---
+++ src/test/java/A_BENIOKUMALISINIZ/BENIOKUMALISINIZ.java
@@ -0,0 +1,51 @@
+package A_BENIOKUMALISINIZ;
+
+public class BENIOKUMALISINIZ {
+    /*
+        *******************************************************************************************************************
+        PROJEYİ CLONELAMAK İÇİN
+
+        --> intellijde iken sırasıyla  FİLE > NEW > PROJECT FROM VERSİON CONTROL adımlalrına tıklıyoruz
+        --> gelen ekranda URL kutucuğuna whatsapptan ve slackten göndereceğim url linkini yapıştırıp CLONE tuşuna basıyoruz.
+
+        *******************************************************************************************************************
+        ÇALIŞMALARIMIZA/KOD YAZMAYA BAŞLAMADAN ÖNCE
+
+        --> çalışmalarımızı kendi branchimizde yapacağız.
+            Bu sebeple öncelikle kendimize bir branch oluşturmalıyız, branch oluşturmak için komutumuz "git branch isim" 'dir.
+            ORN; git branch sumeyye
+
+        --> Kendi branchimize geçmeyi unutmayalım. Hangi branchte olduğumuzu kontrol etmek için komutumuz "git branch" 'tir.
+            hangi branchte isek o branchin yanında * (yıldız) işareti bulunur.
+            ORN; git branch
+            ORNEK CIKTI ; * main
+
+        --> branchler arasında geçiş yapabilmek için komutumuz "git checkout branchİsmi" 'dir.
+            ORN; git branch main (main branchine geçer)
+            ORN; git branch sumeyye (sumeyye branchine geçer)
+
+        --> branchimizi açıp, kendi branchimize geçiş yaptıktan sonra adımızın yazdığı package'tan soru çözümlerine başlayabiliriz
+        --> utilities package'mızda gerekli olan after ve before methodları mevcuttur.
+        --> utilities package'ında değişiklik yapmayınız.
+
+      *******************************************************************************************************************
+      ÇALIŞMALARIMIZ/KOD YAZMA İŞLEMLERİMİZ BİTTİKTEN SONRA
+
+        --> kendi branchimizde iken sırasıyla
+                git add .
+                git commit -m"mesajınız"  mesela git commit -m"28.09.2022"
+                git push
+        (eğer git push komutundan sonra hata alırsanız terminaldeki hata kısmında "git push --set upstream"
+        kısmını kopyalayıp yapıştırarak enter diyoruz, bu işlemi yalnızca 1 defa yapıyoruz. bir sonraki
+        push işlemi için yapmayacağız.)
+
+      -->Projemizi gitHub'a göndermiş olduk
+      * bu aşamadan sonra projemizin sağlıklı çalışabilmesi için lütfen herhangi birşey yapmayalım.
+
+      *******************************************************************************************************************
+
+
+
+
+     */
+}
